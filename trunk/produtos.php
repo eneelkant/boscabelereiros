d2deeacc5d5f42c8caf568615fbb79510b1f9047
rodrigosetti <rodrigosetti@38bde6b2-d133-0410-a0a3-05fd41226e6e>
2007-06-28 22:46:43 +0000

    Adicionados alguns dados a tabela Produto(script sql) + imagens de produtos produtos.php funcionando

--- trunk/produtos.php
+++ trunk/produtos.php
@@ -2,6 +2,31 @@
 
 /* definicoes */
 $produtos_por_pagina = 5;
+$pag = 1;
+
+
+function printNavegador($produtos, $pag, $produtos_por_pagina)
+{
+	$anterior = $pag-$produtos_por_pagina;
+	$proximo = $pag+$produtos_por_pagina;
+
+	/* Imprime anterior e proximo, se houver */
+	echo "<div class='navegador'>\n";
+	if ($anterior < 1 && $pag > 1)
+		$anterior = 1;
+
+	$produtos->getByID($anterior);
+	if ($produtos->getResult())
+	{
+		echo "<a class='anterior' href='produtos.php?pag=$anterior'>&laquo; Anterior</a>\n";
+	}
+	$produtos->getByID($proximo);
+	if ($produtos->getResult())
+	{
+		echo "<a class='proximo' href='produtos.php?pag=$proximo'>Proximo &raquo;</a>\n";
+	}
+	echo "</div>\n";
+}
 
 /* inclui */
 include_once("pagina.php");
@@ -9,6 +34,7 @@ include_once("crud-produtos.php");
 
 /* instancia pagina */
 $pagina = new Pagina("Produtos");
+$pagina->adicionarCSS("style/produtos.css");
 
 /* instancia banco */
 $produtos = new CrudProduto();
@@ -16,43 +42,25 @@ $produtos = new CrudProduto();
 /* Inicia corpo do conteudo da pagina */
 $pagina->inicio("Produtos");
 
-
 /* verifica qual pagina ira imprimir */
 if ($_GET['pag'])
 	$pag = $_GET['pag'];
-else
-	$pag = 1;
+
+printNavegador($produtos, $pag, $produtos_por_pagina);
 
 /* pega do banco os produtos */
-$produtos->getByIDRange($pag, $pag + $produtos_por_pagina);
+$produtos->getByIDRange($pag, $pag + $produtos_por_pagina - 1);
 
 /* Imprime como lista de produtos */
 echo "<ul id='produtos'>\n";
 while ($item = $produtos->getResult()) {
 	
-	echo "\t<li><img src=" . $item["imagem"] . ">" . $item["nome"] . "</img><h1>" . $item["nome"] . 
-	"</h1><p class='descricao'>" . $item["descricao"] . "</p><p class='preco'>" . $item["preco"] . "</p></li>\n";
+	echo "\t<li><img src='img/produtos/" . $item["imagem"] . "' /><h1>" . $item["nome"] . 
+	"</h1><p class='descricao'>" . $item["descricao"] . "</p><p class='preco'> R$ " . $item["preco"] . "</p></li>\n";
 }
 echo "</ul>\n";
 
-/* Imprime anterior e proximo, se houver */
-echo "<div id='navegador'>\n";
-
-$anterior = $pag-1;
-$proximo = $pag+1;
-
-$produtos->getByID($anterior);
-if ($produtos->getResult())
-{
-	echo "<a id='anterior' src='produtos.php?pag=$anterior'>Anterior</a>\n";
-}
-$produtos->getByID($proximo);
-if ($produtos->getResult())
-{
-	echo "<a id='anterior' src='produtos.php?pag=$proximo'>Proximo</a>\n";
-}
-
-echo "</div>\n";
+printNavegador($produtos, $pag, $produtos_por_pagina);
 
 $pagina->fim();
 
---
+++ trunk/src/popular-produtos.sql
@@ -0,0 +1,14 @@
+INSERT INTO boscabelereiros.Produto VALUES
+	(null, 'Chinese Herbal', 'Shampoo para cabelo', 2.20, 'chinese-herbal.jpeg'),
+	(null, 'Herbalife Aloés', 'Shampoo hidratante', 6.30, 'herbalife-aloes.jpeg'),
+	(null, 'Sedal Shampoo', 'Shampoo e Condicionador', 9.90, 'sedal.jpeg'),
+	(null, 'Baby Querubim', 'Condicionador para bebes', 3.10, 'baby.jpeg'),
+	(null, 'Jaborandi', 'Condicionador anti-queda', 10.90, 'jaborandi.jpeg'),
+	(null, 'Notaris', 'Shampoo anti-caspa', 15.20, 'notaris.jpeg'),
+	(null, 'Clean & Clean', 'Sabonete liquido', 2.20, 'sabao-liquido.jpeg'),
+	(null, 'Baby Soap', 'Sabonetes em forma de bebes', 7.15, 'baby-soap.jpeg'),
+	(null, 'Zirh Gel', 'Gel para cabelo', 4.00, 'zirh.jpeg'),
+	(null, 'Colorly', 'Tintura para cabelo', 9.90, 'colorly.jpeg'),
+	(null, 'Dog Color', 'Tintura para cachorros', 10.90, 'dog-color.jpeg'),
+	(null, 'Lapidart Maçarico', 'Maçarico portatil', 54.00, 'macarico.jpeg');
+
