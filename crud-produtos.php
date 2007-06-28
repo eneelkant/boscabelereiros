8cf64c53e6f13c9a39f560db1d4f7401d978125a
rodrigosetti <rodrigosetti@38bde6b2-d133-0410-a0a3-05fd41226e6e>
2007-06-28 15:26:34 +0000

    correcaod e bugs

--- crud-produtos.php
+++ crud-produtos.php
@@ -13,38 +13,41 @@ class CrudProduto extends Banco {
 
 	public function getTodos()
 	{
-		this->getQuery("SELECT * FROM boscabelereiros.Produto");
+		$this->getQuery("SELECT * FROM boscabelereiros.Produto");
 	}
 
 	public function getByName($match)
 	{
-		this->getQuery("SELECT * FROM boscabelereiros.Produto WHERE nome LIKE '%$match%'");
+		$this->getQuery("SELECT * FROM boscabelereiros.Produto WHERE nome LIKE '%$match%'");
 	}
 
 	public function getByID($id)
 	{
-		this.getQuery("SELECT * FROM boscabelereiros.Produto WHERE id = $id");
+		$this.getQuery("SELECT * FROM boscabelereiros.Produto WHERE id = $id");
 	}
 
 	/**** Remove produto pelo id *******/
 
 	public function remover($id)
 	{
-		this->getQuery("DELETE FROM boscabelereiros.Produto WHERE id = $id");
+		$this->getQuery("DELETE FROM boscabelereiros.Produto WHERE id = $id");
 	}
 
 	/***** Altera produto existente *******/
 
 	public function alterar($id, $array)
 	{
-		this->getQuery("UPDATE boscabelereiros.Produto SET nome=$array['nome'], descricao=$array['descricao'], preco=$array['preco'], imagem=$array['imagem']");
+		$this->getQuery("UPDATE boscabelereiros.Produto SET nome= " . $array['nome'] .
+						", descricao= " . $array['descricao'] . ", preco= " . $array['preco'] . 
+						", imagem= " . $array['imagem'] );
 	}
 
 	/****** Adiciona novo produto ********/
 
 	public function adicionar($array)
 	{
-		this->getQuery("INSERT INTO boscabelereiros.Produto VALUES (null, $array['nome'], $array['descricao'], $array['preco'], $array['imagem'])");
+		$this->getQuery("INSERT INTO boscabelereiros.Produto VALUES (null, " . $array['nome'] . "
+						, " . $array['descricao'] . ", " . $array['preco'] . ", " . $array['imagem']);
 	}
 }
 
