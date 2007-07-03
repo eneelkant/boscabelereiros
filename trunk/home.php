7c0bf9697c27690b52d3e7e19044baf372ed09ad
lucasgameiro <lucasgameiro@38bde6b2-d133-0410-a0a3-05fd41226e6e>
2007-07-03 21:16:03 +0000

    arrumei mais um pouco do css

--- trunk/home.php
+++ trunk/home.php
@@ -5,9 +5,13 @@
 	$home = new Pagina("Home");
 	
 	$home->adicionarCSS('home.css');
+
+	$home->adicionarCSS('menu.css');
 	
-	$home->inicio("Sobre a empresa");
-	
+	$home->inicio("Sobre a empresa");?>
+<p class="descricao">Lorem ipsum dolor sit amet, metus ultricies nibh phasellus tincidunt, wisi tellus pulvinar. Nulla condimentum est. Dolor suspendisse neque temporibus, quam tellus pede sit adipiscing varius, elit dictum, diam ac et pede tincidunt nunc qui, pharetra nam metus lacus tincidunt. Mi vestibulum dui. Pede ut nec tortor amet, amet est lacus pulvinar, leo vel ante vitae, et sapien rutrum id adipiscing magnis. Nullam nobis vel eros et, eum tortor pretium.</p>
+<p class="descricao">Sed et massa suspendisse at enim. Vestibulum mauris, libero semper non. Maecenas id fusce suscipit suspendisse justo ante, vestibulum sollicitudin pellentesque cursus, fames fusce cursus, erat accumsan eros dignissim ullamcorper, dui sollicitudin felis. Itaque nunc, felis wisi condimentum dolor aliquam sed ac. Convallis quis, hac sit metus etiam eget. Pellentesque sit, blandit fringilla ante aenean velit eget, turpis mus morbi id fringilla sed, suspendisse egestas ac hendrerit libero nullam accumsan.</p>
+<?php
 	$home->fim();
 
 ?>
