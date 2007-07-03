6da76e009518fbd654fc4d5797aeb670ecebe6d0
leandroboscariol <leandroboscariol@38bde6b2-d133-0410-a0a3-05fd41226e6e>
2007-07-03 23:13:49 +0000

    arrumado o conflito q deu, e mais algumas outras coisas q nao funcionavam antes, dei uma debugada

--- crud-usuarios.php
+++ crud-usuarios.php
@@ -11,18 +11,18 @@ class CrudUsuario extends Banco
 	public function adicionar( $cliente )
 	{
 		$this->getQuery( "INSERT INTO Pessoa VALUES 
-					( null, 
-					$cliente['nome'], 
-					$cliente['login'], 
-					$cliente['senha'], 
+					( null,", 
+					$cliente['nome'],
+					$cliente['login'],
+					$cliente['senha'],
 					$cliente['email'],
 					$cliente['rg'],
 					$cliente['cpf'],
 					$cliente['telefone_res'],
-					$cliente['telefone_cel'] ); " );
+					$cliente['telefone_cel']," ); " );
 		$this->getQuery("SELECT AUTO_INCREMENT FROM information_schema.`TABLES` WHERE TABLE_NAME LIKE 'Pessoa'");
 		$resultado = $this->getResult();
-		$this->getQuery("INSERT INTO Cliente VALUES ( NULL, ".$resultado['id']-1.")");
+		$this->getQuery("INSERT INTO Cliente VALUES ( NULL, ",$resultado['id']-1,")");
 	/*lembrar de adicionar tb na tabela cliente*/
 	//SELECT TABLE_NAME,AUTO_INCREMENT FROM information_schema.`TABLES` WHERE TABLE_NAME LIKE 'Pessoa'
 	// query acima usada para obter o indice do AUTO_INCREMENT, ou seja, o ultimo dado inserido	
@@ -39,7 +39,7 @@ class CrudUsuario extends Banco
 	{/*tambem vai precisar saber qual é o id da pessoa, o id recebido é do cliente*/
 		$this->getQuery( "SELECT pessoa FROM Cliente WHERE id = $id" );
 		$resultado = $this->getResult();
-		$this->getQuery( "UPDATE Pessoa WHERE id = $resultado['pessoa'] 
+		$this->getQuery( "UPDATE Pessoa WHERE id = ",$resultado['pessoa']," 
 			SET login = " . $cliente['login'] . 
 			"senha = " . $cliente['senha'] . 
 			"nome = " . $cliente['nome'] . 
@@ -50,6 +50,11 @@ class CrudUsuario extends Banco
 			"email = " . $cliente['email'] );
 	}
 	
+	public function getByNome( $nome )
+	{
+		$this->getQuery( "SELECT boscabelereiros.Cliente.id, boscabelereiros.Pessoa.* FROM boscabelereiros.Cliente INNER JOIN boscabelereiros.Pessoa ON boscabelereiros.Cliente.pessoa = boscabelereiros.Pessoa.id WHERE boscabelereiros.Pessoa.nome LIKE $nome" );
+	}
+
 	/* lista os dados por cliente de acordo com seu id */
 	public function listaDados( $id )
 	{
